5fd2f644bc188bf3da202969652cd9f1b671bb85
new_block <noviy_block@mail.ru>
2024-01-17 23:55:55 +0300

    Rewrite all define methods in one define method

--- src/card/poker_hand/Hand.java
+++ src/card/poker_hand/Hand.java
@@ -8,7 +8,6 @@ import java.util.List;
 
 public class Hand {
     protected List<Card> cards;
-    protected int combinationRang;
 
     public Hand() {
         this.cards = new ArrayList<>();
@@ -26,71 +25,25 @@ public class Hand {
         return "";
     }
 
-    //Определение типа комбинации в руке
-    private int isOnePair(List<Card> hand){
-        OnePairUtil onePairUtil = new OnePairUtil();
-        if(onePairUtil.isMatch(hand)){
-            return onePairUtil.getRank();
-        }
-        return 1;
-    }
-    private int isTwoPairs(List<Card> hand){
-        TwoPairUtil twoPairUtil = new TwoPairUtil();
-        if(twoPairUtil.isMatch(hand)){
-            return twoPairUtil.getRank();
-        }
-        return 0;
-    }
-    private int isSet(List<Card> hand){
-        SetUtil setUtil = new SetUtil();
-        if (setUtil.isMatch(hand)){
-            return setUtil.getRank();
-        }
-        return 1;
-    }
-    private int isStraight(List<Card> hand){
-        StraightUtil straightUtil = new StraightUtil();
-        if(straightUtil.isMatch(hand)){
-            return straightUtil.getRank();
-        }
-        return 1;
-    }
-    private int isFlush(List<Card> hand){
-        FlushUtil flushUtil = new FlushUtil();
-        if(flushUtil.isMatch(hand)){
-            return flushUtil.getRank();
-        }
-        return 1;
-    }
-    private int isFullHouse(List<Card> hand){
-        FullHouseUtil fullHouseUtil = new FullHouseUtil();
-        if(fullHouseUtil.isMatch(hand)){
-            return fullHouseUtil.getRank();
-        };
-        return 1;
-    }
+    private int defineCombination(List<Card> hand) {
+        List<IPokerCombDefineUtil> combinationUtils = new ArrayList<>();
+        combinationUtils.add(new OnePairUtil());
+        combinationUtils.add(new TwoPairUtil());
+        combinationUtils.add(new SetUtil());
+        combinationUtils.add(new StraightUtil());
+        combinationUtils.add(new FlushUtil());
+        combinationUtils.add(new FullHouseUtil());
+        combinationUtils.add(new FourCardsUtil());
+        combinationUtils.add(new StraightFlushUtil());
+        combinationUtils.add(new FlushRoyalUtil());
 
-    private int isFourCards(List<Card> hand){
-        FourCardsUtil fourCardsUtil = new FourCardsUtil();
-        if(fourCardsUtil.isMatch(hand)){
-            return fourCardsUtil.getRank();
-        }
-        return 1;
-    }
+        int maxCombinationRank = 1;
 
-    private int isStraightFlush(List<Card> hand){
-        StraightUtil straightUtil = new StraightUtil();
-        if(straightUtil.isMatch(hand)){
-            return straightUtil.getRank();
+        for (IPokerCombDefineUtil util : combinationUtils) {
+            int rank = util.isMatch(hand) ? util.getRank() : 0;
+            maxCombinationRank = Math.max(maxCombinationRank, rank);
         }
-        return 1;
-    }
 
-    private int isRoyalFlush(List<Card> hand){
-        FlushRoyalUtil flushRoyalUtil = new FlushRoyalUtil();
-        if(flushRoyalUtil.isMatch(hand)){
-            return flushRoyalUtil.getRank();
-        }
-        return 1;
+        return maxCombinationRank;
     }
 }
